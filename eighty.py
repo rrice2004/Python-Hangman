5526b97ce22c51db6483d27a8e6bb38d67eed097
rrice2004 <40141955+rrice2004@users.noreply.github.com>
2021-01-03 16:02:49 -0500

    Create eighty.py

---
+++ eighty.py
@@ -0,0 +1,22 @@
+word_list = [
+'ratt',
+'poison',
+'twisted sister',
+'skid row',
+'guns n roses',
+'slaughter',
+'kix',
+'van halen',
+'tora tora',
+'ozzy osbourne',
+'lita ford',
+'vixen',
+'dangerous toys',
+'blue murder',
+'damn yankees',
+'def leppard',
+'tesla',
+'warrant',
+'motley crue'
+
+]
