4bc6543866661b63c95739eb85d741c78495565f
rrice2004 <40141955+rrice2004@users.noreply.github.com>
2021-01-03 16:02:17 -0500

    Create dc.py

---
+++ dc.py
@@ -0,0 +1,18 @@
+word_list = [
+'batman', 
+'joker', 
+'catwoman',
+'wonder woman',
+'poison ivy'
+'harley quinn',
+'scarecrow',
+'penguin',
+'green arrow',
+'green lantern',
+'flash',
+'barbara gordon',
+'james gordon',
+'black canary',
+'zatanna zatara',
+
+]
